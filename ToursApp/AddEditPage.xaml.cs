5249acd0fd2a8d8ee9923496a06fcbcbf944f0a6
Yu0ne <151286425+Yu0ne@users.noreply.github.com>
2023-11-28 17:34:16 +0800

    Second commit

--- ToursApp/AddEditPage.xaml.cs
+++ ToursApp/AddEditPage.xaml.cs
@@ -20,9 +20,48 @@ namespace ToursApp
     /// </summary>
     public partial class AddEditPage : Page
     {
-        public AddEditPage()
+        private Hotel _currentHotel = new Hotel();
+        public AddEditPage(Hotel selectedHotel)
         {
             InitializeComponent();
+
+            if (selectedHotel != null)
+                _currentHotel = selectedHotel;
+            DataContext = _currentHotel;
+            ComboCountries.ItemsSource = tourbaseEntities.GetContext().Country.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentHotel.Name))
+                errors.AppendLine("Укажите название отеля");
+            if (_currentHotel.CountOfStars < 1 || _currentHotel.CountOfStars > 5)
+                errors.AppendLine("Количество звезд - число от 1 до 5");
+            if (_currentHotel.Country == null)
+                errors.AppendLine("Выберите страну");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentHotel.Id == 0)
+                tourbaseEntities.GetContext().Hotel.Add(_currentHotel);
+            try
+            {
+                tourbaseEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+                
+            }
+
         }
     }
 }
--- ToursApp/HotelsPage.xaml.cs
+++ ToursApp/HotelsPage.xaml.cs
@@ -23,11 +23,54 @@ namespace ToursApp
         public HotelsPage()
         {
             InitializeComponent();
+            //DGridHotels.ItemsSource = tourbaseEntities.GetContext().Hotel.ToList();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var HotelForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {HotelForRemoving.Count()} элементов?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    tourbaseEntities.GetContext().Hotel.RemoveRange(HotelForRemoving);
+                    tourbaseEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+
+                    DGridHotels.ItemsSource = tourbaseEntities.GetContext().Hotel.ToList();
+                }
+
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Hotel));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                tourbaseEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = tourbaseEntities.GetContext().Hotel.ToList();
+            }
         }
     }
 }
